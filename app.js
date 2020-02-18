ed2fe07e6683848e1d934ee7926de8b013acd609
ajinkya12 <dajinkya.12@gmail.com>
2020-02-18 19:03:49 +0530

    auth Token

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ function yaadDilaDo() {
   });*/
   var url = "9hDsZS8nEqe7zEXEI1x9hYyu/8EJUHF4UB/QD7D8E30T/secivres/moc.kcals.skooh//:sptth";
   var actual = url.split("").reverse().join("");
-  
+  var token = "RB7726vi8Rj0bsi0JTDxArc8-846348849659-6649444843-bxox";
+  var actualToken = token.split("").reverse().join("");
+
   $.ajax({
       type: 'POST',
       url: actual,
       data: JSON.stringify({
         "text": "NEW NEW"
       }),
+      headers: {
+        "Authorization": "Basic " + actualToken
+      },
       error: function(e) {
         console.log(e);
       },
