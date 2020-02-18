e84d947e0c621b76238c5a8913309e3eac7c4247
ajinkya12 <dajinkya.12@gmail.com>
2020-02-18 19:38:59 +0530

    something

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ function yaadDilaDo() {
   /* var xhr = $.get('https://www.eventsnow.com/hyderabad/srh-ipl-tickets', function(response){
       //console.log('RESPONSE', response.includes('No matches available currently'));
   });*/
-  var url = "9hDsZS8nEqe7zEXEI1x9hYyu/8EJUHF4UB/QD7D8E30T/secivres/moc.kcals.skooh//:sptth";
+  var url = "EYvs5bLcCHZbNdCTzgRPBWHj/Z36E927UB/QD7D8E30T/secivres/moc.kcals.skooh//:sptth";
   var actual = url.split("").reverse().join("");
-  var token = "RB7726vi8Rj0bsi0JTDxArc8-846348849659-6649444843-bxox";
-  var actualToken = token.split("").reverse().join("");
-
   var text = "NEW NEW";
   $.ajax({
       type: 'POST',
@@ -14,10 +11,6 @@ function yaadDilaDo() {
       data: 'payload=' + JSON.stringify({
         "text": text
       }),
-      error: function(e) {
-        console.log(e);
-      },
-      dataType: "json",
-      contentType: "application/json"
+      dataType: "json"
   });
 };
