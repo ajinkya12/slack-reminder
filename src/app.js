dc0ec4f44c890147f5f800094e8067774df53b53
ajinkya12 <dajinkya.12@gmail.com>
2020-02-18 17:29:54 +0530

    package

--- src/app.js
+++
@@ -1,9 +0,0 @@
-function yaadDilaDo() {
-  /* var xhr = $.get('https://www.eventsnow.com/hyderabad/srh-ipl-tickets', function(response){
-      //console.log('RESPONSE', response.includes('No matches available currently'));
-  });*/
-  $.post('https://hooks.slack.com/services/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',
-                      {'text': 'NEW NEW'}, function(response){
-                        console.log(JSON.stringify(response));
-                      });
-};
